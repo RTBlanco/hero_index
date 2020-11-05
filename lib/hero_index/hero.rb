3eac1f9c40f04b55064a483dbcd26b1f3c80f99c
RTBlanco <ronnytoribio1@hotmail.com>
2020-11-04 19:15:52 -0500

    Added "powerstats=" method and "is_stronger?" method

--- lib/hero_index/hero.rb
+++ lib/hero_index/hero.rb
@@ -1,18 +1,27 @@
 class HeroIndex::Hero 
+  attr_accessor :appearance, :biography, :id, :name
+  attr_reader :powerstats
   
   @@all = []
   def initialize(attri)
     attri.each do |key , value|
-      unless key == "connections" || key == "image" || key == "work" 
-        self.class.attr_accessor(key)
-        self.send(("#{key}="), value)
+      if self.class.method_defined?(key.downcase)          # unless key == "connections" || key == "image" || key == "work" 
+        # binding.pry
+        # self.class.attr_accessor(key)
+        self.send("#{key.downcase}=", value)
       end
     end
     @@all << self
   end
 
-  def versus(hero)
+  def is_stronger?(hero)
+    @powerstats > hero.powerstats
+  end
 
+  def powerstats=(powerstats)
+    binding.pry
+    @powerstats = powerstats.each {|k,v| powerstats[k] = v.to_i }
   end
 
 end
+
